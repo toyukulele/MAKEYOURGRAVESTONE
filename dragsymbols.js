5b9a9dffc6aee9ba11fcaa14d6deaaf781512567
toyukulele <toyukulele@gmail.com>
2021-06-04 21:49:04 +0300

    s

--- dragsymbols.js
+++ dragsymbols.js
@@ -1,23 +1,28 @@
-document.querySelectorAll(".symbols").forEach(function (symbols) {
-    symbols.addEventListener("mousedown", function (e) {
-        symbols.classList.add("is-dragged");
-    });
-});
+var symbols = document.querySelectorAll(".symbol");
 
-window.addEventListener("mousemove", function (event) {
-    var draggedElement = document.querySelector(".is-dragged");
-    
-    if (draggedElement) {
-        var draggedElementRect = draggedElement.getBoundingClientRect();
-        var translateX = event.clientX - draggedElementRect.width / 2;
-        var translateY = event.clientX - draggedElementRect.height / 2;
-        var translate = "translate(" + translateX + "px, " + translateY + "px)";
-     draggedElement.style.transform = translate;
-    }
+symbols.forEach(function (symbol) {
+  symbol.addEventListener("mousedown", function (e) {
+    symbol.classList.add("is-dragged");
+  });
 });
 
 window.addEventListener("mouseup", function () {
-    document.querySelectorAll(".symbols").forEach(function (symbols) {
-       symbols.classList.remove("is-dragged");
-    });
+  symbols.forEach(function (symbol) {
+    symbol.classList.remove("is-dragged");
+  });
+});
+
+window.addEventListener("mousemove", function (event) {
+  var draggedElement = document.querySelector(".is-dragged");
+
+  if (draggedElement) {
+    var draggedElementRect = draggedElement.getBoundingClientRect();
+    var translateX =
+      event.clientX - draggedElementRect.left + draggedElementRect.width / 2;
+    var translateY =
+      event.clientY - draggedElementRect.top + draggedElementRect.height / 2;
+    console.log(translateX, translateY);
+    var translate = "translate(" + translateX + "px, " + translateY + "px)";
+    draggedElement.style.transform = translate;
+  }
 });
