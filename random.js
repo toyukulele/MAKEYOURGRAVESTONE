91fa1a63db08ecf7cf43a9c7e6cc03ffb8fc0290
toyukulele <toyukulele@gmail.com>
2021-06-08 23:46:06 +0300

    k

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 
-function generator() {
+function button() {
   for (let index = 0; index < 10; index++) {
-    var x = Math.floor(Math.random() * 6 + 1);
+    var x = Math.floor((Math.random()*6)+1);
     console.log(x);
-    document.getElementById("divImage").innerHTML += `
+    document.getElementById("divImage").innerHTML+= `
        <img src="images/g${x}.png" style="width:100px;" >
    `;
   }
