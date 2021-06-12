94d22ca0444fe575d564154fa07c6d08bb825fe0
toyukulele <toyukulele@gmail.com>
2021-06-13 01:09:34 +0300

    symbols

--- random.js
+++ random.js
@@ -1,13 +1,117 @@
-
-function button() {
+function showTombstones() {
   for (let index = 0; index < 10; index++) {
-    var x = Math.floor((Math.random()*6)+1);
+    var x = Math.floor(Math.random() * 6 + 1);
     console.log(x);
-    document.getElementById("divImage").innerHTML+= `
+    document.getElementById("divImage").innerHTML += `
        <img src="images/g${x}.png" style="width:100px;" >
    `;
   }
 }
+
+var symbols = [
+  {
+    imgPath: "6.svg", text: "Знания", },
+  {
+    imgPath: "47.svg", text: "Победы", },
+  {
+    imgPath: "8.svg", text: "Дом", },
+  {
+    imgPath: "9.svg", text: "Дружелюбный", }, 
+  {
+    imgPath: "10.svg", text: "Одиночка", },
+  {
+    imgPath: "11.svg", text: "Адаптивный", },
+  {
+    imgPath: "12.svg", text: "Спокойствия", },
+  {
+    imgPath: "13.svg", text: "Процветания", }, 
+  {
+    imgPath: "14.svg", text: "Заботливый", },
+  {
+    imgPath: "15.svg", text: "Вдумчивый", },
+  {
+    imgPath: "16.svg", text: "Вечной памяти", },
+  {
+    imgPath: "17.svg", text: "Прощения", }, 
+  {
+    imgPath: "18.svg", text: "Вечности", },
+  {
+    imgPath: "19.svg", text: "Радости", },
+  {
+    imgPath: "20.svg", text: "Хорошей жизни", },
+  {
+    imgPath: "21.svg", text: "Целеустремленный", }, 
+  {
+    imgPath: "22.svg", text: "Душа компании", },
+  {
+    imgPath: "23.svg", text: "Веселье", },
+  {
+    imgPath: "24.svg", text: "Воскрешения", },
+  {
+    imgPath: "25.svg", text: "Здоровья", }, 
+  {
+    imgPath: "26.svg", text: "Пессимист", },
+  {
+    imgPath: "27.svg", text: "Вдохновения", },
+  {
+    imgPath: "28.svg", text: "Риск", },
+  {
+    imgPath: "29.svg", text: "Стойкий", }, 
+  {
+    imgPath: "30.svg", text: "Соулмейт", },
+  {
+    imgPath: "31.svg", text: "Развитие", },
+  {
+    imgPath: "32.svg", text: "Семьянин", },
+  {
+    imgPath: "33.svg", text: "Дом", }, 
+    {
+    imgPath: "34.svg", text: "Уверенности", },
+  {
+    imgPath: "35.svg", text: "Гармонии", },
+  {
+    imgPath: "36.svg", text: "Светлое будущее", },
+  {
+    imgPath: "37.svg", text: "Самостоятельный", }, 
+  {
+    imgPath: "38.svg", text: "Смелый", },
+  {
+    imgPath: "39.svg", text: "Счастья", },
+  {
+    imgPath: "40.svg", text: "Новое начало", },
+  {
+    imgPath: "41.svg", text: "Стабильность", }, 
+  {
+    imgPath: "42.svg", text: "Приключения", },
+  {
+    imgPath: "43.svg", text: "Дружелюбный", },
+  {
+    imgPath: "44.svg", text: "Дети", },
+  {
+    imgPath: "45.svg", text: "Карьера", }, 
+  {
+    imgPath: "46.svg", text: "Внимательный", },
+];
+
+var symbolsBox = document.querySelector(".symbols");
+symbols.forEach(function (symbol, index) {
+  const symbolEl = document.createElement("div");
+  symbolEl.classList.add("symbol", "symbols-" + (index + 1));
+
+  var img = document.createElement("img");
+  img.src = symbol.imgPath;
+
+  var symbolText = document.createElement("div");
+  symbolText.innerHTML = symbol.text;
+  symbolText.classList.add("symbol-text");
+
+  symbolEl.appendChild(img);
+  symbolEl.appendChild(symbolText);
+  symbolsBox.appendChild(symbolEl);
+});
+
+var randomButton = document.querySelector(".button");
+randomButton.addEventListener("click", showTombstones);
 
 var causeOfDeathRow = document.querySelector(".cause-of-death-row");
 var targetGrave = document.querySelector(".big-grave");
@@ -20,6 +124,7 @@ var causesOfDeath = [
   { imgPath: "grave4.png", text: "Старость" },
   { imgPath: "grave5.png", text: "Болезнь" },
 ];
+
 
 causesOfDeath.forEach(function (causeOfDeath) {
   var column = document.createElement("div");
@@ -41,7 +146,6 @@ causesOfDeath.forEach(function (causeOfDeath) {
   column.appendChild(columnText);
   causeOfDeathRow.appendChild(column);
 });
-
 
 var placeOfDeathRow = document.querySelector(".place-of-death-row");
 var targetGrave = document.querySelector(".big-grave");
